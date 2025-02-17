3c5a31d2d4fe50c64f17afa3a7f09067915a9b8b
Lucile <lucilelafonr@gmail.com>
2025-02-17 15:14:29 +0100

    Revert "Create sales_daily.sql"

--- sales_daily.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-    sale_date, 
-    ROUND(SUM(turnover), 2) AS daily_turnover, 
-    ROUND(SUM(purchase_cost), 2) AS daily_purchase_cost
-FROM sales
-GROUP BY sale_date
-ORDER BY sale_date;
