7dacfcc952b520c82da801e73c738f97533d8e2f
S3aal <lucilelafonr@gmail.com>
2025-02-17 16:16:40 +0100

    discount
    discount

--- sales_daily.sql
+++ sales_daily.sql
@@ -1,7 +1,7 @@
 SELECT 
     sale_date, 
     ROUND(SUM(turnover), 2) AS daily_turnover, 
-    ROUND(SUM(discount), 2) AS daily_purchase_cost
+    ROUND(SUM(discount), 2) AS daily_purchase_cost,
 FROM sales
 GROUP BY sale_date
 ORDER BY sale_date;
